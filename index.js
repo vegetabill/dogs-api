31cb734e397b6a4eb3e9624fd6d6fbbd28c5b6d3
Bill D <355438+vegetabill@users.noreply.github.com>
2020-03-08 13:26:13 -0700

    clearer separation of app vs express overhead

---
+++ index.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+// npm packages
+const express = require('express');
+const morgan = require('morgan');
+const pgp = require('pg-promise')();
+
+// project modules
+const { setupApp } = require('./src/app');
+const { printRoutes } = require('./src/utils');
+
+// Config
+const DEFAULT_DB_NAME = 'dogs-dev';
+const DB_NAME = process.env.DB_NAME || DEFAULT_DB_NAME;
+const connectionString = `postgres://postgres@localhost:5432/${DB_NAME}`;
+const db = pgp(connectionString);
+console.log('Postgres DB => ', connectionString);
+
+const DEFAULT_PORT = 3000;
+const PORT = process.env.PORT || DEFAULT_PORT;
+
+const app = express();
+app.use(morgan('dev'));
+
+setupApp(app, db).then(() => {
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+  // eslint-disable-next-line no-underscore-dangle
+  printRoutes(`http://localhost:${PORT}`, app._router.stack);
+});
---
+++ src/app.js
@@ -0,0 +1,43 @@
+const { camelize } = require('./utils');
+const { createNotAllowedHandler } = require('./response-utils');
+const BreedsTable = require('./breeds-table');
+const DogsTable = require('./dogs-table');
+
+function setupApp(app, db) {
+  // DB Tables
+  const breeds = new BreedsTable(db);
+  const dogs = new DogsTable(db);
+
+  const sendData = (res, data) => {
+    res.send(camelize(data));
+  };
+
+  app.route('/dogs').get((req, res) => {
+    dogs.all().then(data => sendData(res, data));
+  });
+
+  app
+    .route('/breeds')
+    .get((_, res) => {
+      breeds.all().then(data => sendData(res, data));
+    })
+    // We'll just consider breeds an immutable list for now
+    // So any non-GET is 405
+    .post(createNotAllowedHandler())
+    .put(createNotAllowedHandler())
+    .delete(createNotAllowedHandler());
+
+  return new Promise((resolve, reject) => {
+    return Promise.all([breeds.sanityCheck(), dogs.sanityCheck()])
+      .then(() => {
+        resolve(true);
+      })
+      .catch(e => {
+        // eslint-disable-next-line no-console
+        console.error('unable to connect to database. see README for troubleshooting');
+        reject(e);
+      });
+  });
+}
+
+module.exports = { setupApp };
